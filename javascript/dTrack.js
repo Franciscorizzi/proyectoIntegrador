5051ffa9552107c3ff30413051b18f4158161aff
Agustina Bierfass <agustinabierfass@Agustinas-MacBook-Pro.local>
2020-06-08 20:56:47 -0300

    vaaaaaaaa

--- javascript/dTrack.js
+++ javascript/dTrack.js
@@ -3,4 +3,44 @@ let datos = new URLSearchParams (queryString);
 let idTrack = datos.get ('id');
 console.log(idTrack);
 
-let proxy = 'https://cors-anywhere.herokuapp.com/'
+let proxy = 'https://cors-anywhere.herokuapp.com/' ;
+let url = proxy + 'https://api.deezer.com/track/' + idTrack
+
+fetch(url)
+ .then(function(response){
+     return response.json();
+ })
+ .then(function(data){
+     console.log(data);
+     let trackT = document.querySelector ('.trackT');
+     trackT.innerHTML += data.type;
+     trackT.style.textTransform = "uppercase" ;
+
+     let nombreT = document.querySelector('.nombreT') ;
+     nombreT.innerHTML = data.title ;
+
+     let artista = document.querySelector ('.artista') ;
+     artista.innerHTML = "<a href= '../HTML/dArtista.html?id=" + data.artist.id + "'>" + 
+     "<h5>" + data.artist.name + "</h5> </a>"
+
+     let fecha = document.querySelector('.fechaT') ;
+     fecha.innerHTML = data.release_date;
+     
+     let fotoGrande = document.querySelector ('.fotoAlbumT') ;
+     fotoGrande.innerHTML = "<img src= '" + data.album.cover_big + "' alt='foto'></img> " ;
+
+     let fotoChica = document.querySelector ('.album') ;
+     fotoChica.innerHTML = "<a href'dAlbum.html?id= " + data.album.id + "'> " + 
+     "<img src = '" + data.album.cover_medium + "' alt= 'foto'>" + "<br>" + " <h5> "
+     + data.album.title + "</h5> " +" </a>" ;
+      
+
+
+
+
+
+
+
+
+
+ })
