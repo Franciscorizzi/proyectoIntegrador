5c842c33620f69dadee605ba593a782fef88b1a0
Agustina Bierfass <agustinabierfass@Agustinas-MacBook-Pro.local>
2020-06-10 18:48:15 -0300

    linkeado

--- javascript/dTrack.js
+++ javascript/dTrack.js
@@ -17,7 +17,8 @@ fetch(url)
      trackT.style.textTransform = "uppercase" ;
 
      let nombreT = document.querySelector('.nombreT') ;
-     nombreT.innerHTML = data.title ;
+     nombreT.innerHTML =  "<a href= '../HTML/dAlbum.html?id=" + data.title.id + "'>" +
+     "<h5>" + data.title + " </h5> </a>"
 
      let artista = document.querySelector ('.artista') ;
      artista.innerHTML = "<a href= '../HTML/dArtista.html?id=" + data.artist.id + "'>" + 
@@ -27,50 +28,125 @@ fetch(url)
      fecha.innerHTML = data.release_date;
      
      let fotoGrande = document.querySelector ('.fotoAlbumT') ;
-     fotoGrande.innerHTML = "<img src= '" + data.album.cover_big + "' alt='foto'></img> " ;
+     fotoGrande.innerHTML = "<a href = '../HTML/dAlbum.html?id= " + data.album.id + "'> " + 
+      "<img src= '" + data.album.cover_big + "' alt='foto'>" + "<br>" + " <h5> "
+      + data.album.title + "</h5> " +" </a> </img> " ;
 
      let fotoChica = document.querySelector ('.album') ;
-     fotoChica.innerHTML = "<a href'dAlbum.html?id= " + data.album.id + "'> " + 
+     fotoChica.innerHTML = "<a href = '../HTML/dAlbum.html?id= " + data.album.id + "'> " + 
      "<img src = '" + data.album.cover_medium + "' alt= 'foto'>" + "<br>" + " <h5> "
      + data.album.title + "</h5> " +" </a>" ;
-     
-     //let player = document.querySelector('iframe') ;
-     //player.src = 'https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=007FEB&layout=dark&size=medium&type=tracks&id=' + idTrack + '&app_id=1'
+     //
+     //  let player = document.querySelector('iframe') ;
+     //  player.src = 'https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=007FEB&layout=dark&size=medium&type=tracks&id=' + idTrack + '&app_id=1'
      
 
      let playlist = [data]
      let recuperoStorage = localStorage.getItem('playlist') ;
 
-     
      let agregar = document.querySelector('.botT') ;
      agregar.addEventListener('click', function(){
         if(recuperoStorage === null){
             window.localStorage.setItem('playlist',JSON.stringify(playlist)) ;
         }else{
             let nuevaPlaylist = JSON.parse(recuperoStorage) 
-   
-   
             nuevaPlaylist.push(data) ;
             window.localStorage.setItem('playlist',JSON.stringify(nuevaPlaylist))
             
         }
         if(playlist.includes(idTrack)){
-                     let indiceEnElArray = playlist.indexOf(idTrack);
-                     playlist.splice(indiceEnElArray, 1);
-                     document.querySelector('.botT').innerHTML = 'ADD TO PLAYLIST' ;
-                     console.log(playlist);
-                 }else{
-                     playlist.push(idTrack);
-                     document.querySelector('.botT').innerHTML = 'REMOVE FROM PLAYLIST' ;
-            
-                 }
-            
-
+            let indiceEnElArray = playlist.indexOf(idTrack);
+            playlist.splice(indiceEnElArray, 1);
+            document.querySelector('.botT').innerHTML = 'ADD TO PLAYLIST' ;
+            console.log(playlist);
+        }else{
+            playlist.push(idTrack);
+            document.querySelector('.botT').innerHTML = 'REMOVE FROM PLAYLIST' ;
+        }
         agregar.innerHTML = 'REMOVE FROM PLAYLIST';
         console.log(JSON.parse (recuperoStorage))
 
 
      })
+
+        
+
+   
+
+
+
+
+
+    //  if(recuperoStorage === null){
+    //      playlist = [] ;
+    //  }else{
+    //      playlist = JSON.parse(recuperoStorage) ;
+    //  }
+    //  if(playlist.includes(idTrack)){
+    //      document.querySelector('.botT').innerHTML = 'REMOVE FROM PLAYLIST'
+    //  }
+    //Ale
+    //  let agregar = document.querySelector('.botT') ;
+    //  agregar.addEventListener('click', function(e){
+    //      e.preventDefault(); 
+    //      if(playlist.includes(idTrack)){
+    //          let indiceEnElArray = playlist.indexOf(idTrack) ;
+    //          playlist.splice(indiceEnElArray, 1) ;
+    //          document.querySelector('.agregar').innerHTML = 'ADD TO PLAYLIST' ;
+    //          console.log(playlist) ;
+    //      }else{
+    //          playlist.push(idTrack) ;
+    //          document.querySelector('.agregar').innerHTML = 'REMOVE FROM PLAYLIST'
+    //      }
+    //      let playlistParaStorage = JSON.stringify(playlist) ;
+    //      localStorage.setItem('playlist', playlistParaStorage) ;
+    //      console.log(localStorage) ;
+    //  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+    //  let agregar = document.querySelector('.botT') ;
+    //  agregar.addEventListener('click', function(){
+    //     if(recuperoStorage === null){
+    //         window.localStorage.setItem('playlist',JSON.stringify(playlist)) ;
+    //     }else{
+    //         let nuevaPlaylist = JSON.parse(recuperoStorage) 
+   
+   
+    //         nuevaPlaylist.push(data) ;
+    //         window.localStorage.setItem('playlist',JSON.stringify(nuevaPlaylist))
+            
+    //     }
+    //     if(playlist.includes(idTrack)){
+    //                  let indiceEnElArray = playlist.indexOf(idTrack);
+    //                  playlist.splice(indiceEnElArray, 1);
+    //                  document.querySelector('.botT').innerHTML = 'ADD TO PLAYLIST' ;
+    //                  console.log(playlist);
+    //              }else{
+    //                  playlist.push(idTrack);
+    //                  document.querySelector('.botT').innerHTML = 'REMOVE FROM PLAYLIST' ;
+            
+    //              }
+            
+
+    //     agregar.innerHTML = 'REMOVE FROM PLAYLIST';
+    //     console.log(JSON.parse (recuperoStorage))
+
+
+    //  })
      
 
 
--- javascript/playlist.js
+++ javascript/playlist.js
@@ -1,7 +1,7 @@
 let recuperoStorage = localStorage.getItem('playlist');
 let playlist = JSON.parse(recuperoStorage);
 let playlistWrapper = document.querySelector('.playlistWrapper') ;
-
+console.log(playlist) ;
 
 if(recuperoStorage == null || recuperoStorage.length <= 0){
     playlist = [] ;
@@ -9,16 +9,57 @@ if(recuperoStorage == null || recuperoStorage.length <= 0){
     console.log(playlistWrapper) ; 
 }else{
     playlist.forEach(track => {
-
-        playlistWrapper.innerHTML += '<li>' + '<a href="dTrack.html?id=' + track.id + '">' +  '<audio controls><source src="'+ track.preview +'"></source> </audio>  </li>'
-       
-    });
-        
+            playlistWrapper.innerHTML += `<li>  <a href="dTrack.html?id= ${track.id } "> 
+              <audio controls><source src="${track.preview}"> </source> </audio> </li>`
+        });
 }
 let clear = document.querySelector('.clear')
 clear.addEventListener('click', function(){
     window.localStorage.clear('playlist')
 })
+
+
+
+
+
+
+
+
+// if(recuperoStorage == null || recuperoStorage.length <= 0){
+//     playlist = [] ;
+//     playlistWrapper.innerHTML += '<li> No hay canciones </li>' 
+//     console.log(playlistWrapper) ; 
+// }else{
+//     playlist.forEach(function(idTrack){
+//         buscarYMostrarTrack(idTrack) ;
+//     }) ;
+// }
+// function buscarYMostrarTrack(idTrack){
+//     let proxy = 'https://cors-anywhere.herokuapp.com/' ;
+//     let url = proxy + 'https://api.deezer.com/track/' + idTrack ;
+//     fetch(url)
+//     .then(function(response){
+//         return response.json() ;
+//     })
+//     .then(function (trackT){
+//         playlistWrapper.innerHTML+= '<li>' + '<a href="track.html?id=' + track.id + '">' + track.title + '</li>'
+//     })
+//     .catch(function(errors){
+//         console.log(errors) ;
+//     })
+
+// }
+//------------
+
+    // playlist.forEach(trackT => {
+    //     playlistWrapper.innerHTML += '<li>' + '<a href="track.html?id=' + track.id + '">' +  '<audio controls><source src="'+track.preview +'"type="audio/mp3"></audio> '+ '</li>'
+    // })
+        
+
+// let clear = document.querySelector('.clear')
+// clear.addEventListener('click', function(){
+//     window.localStorage.clear('playlist')
+// })
 //  function buscarYMostrarTrack(){
      
 //      return playlistWrapper.innerHTML += '<li>' + '<a href="track.html?id=' + this.id + '">' + this.title + '</li>'
